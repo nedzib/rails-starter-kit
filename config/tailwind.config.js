3f47f6f31ca92f25e5b6d7f32de5be829821d1aa
Nedzib Sastoque <bsastoque@buk.co>
2023-09-01 11:52:43 -0500

    feat: landing and colors

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,9 +9,23 @@ module.exports = {
     './app/components/**/*.{erb,haml,html,slim}',
   ],
   theme: {
-    extend: {
+    colors: {
+      transparent: 'transparent',
+      background: '#004643',
+      headline: '#fffffe',
+      paragraph: '#abd1c6',
+      button: '#f9bc60',
+      button_text: '#001e1d',
+      stroke: '#001e1d',
+      main: '#e8e4e6',
+      highlight: '#f9bc60',
+      secondary: '#abd1c6',
+      tertiary: '#e16162',
     },
   },
   plugins: [
   ]
 }
+
+// https://www.happyhues.co/palettes/10
+// https://bgjar.com/
